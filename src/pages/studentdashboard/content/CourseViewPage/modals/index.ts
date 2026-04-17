e012e269f475b6b81f45a263eea8ff000ff48b11
Stranger2323 <josiahdeasis009@gmail.com>
2026-04-17 15:20:02 +0800

    Phase 1.1: Extract AddTaskModal from CourseViewPage (6467 -> 6042 lines) - all 3 modals extracted

--- src/pages/studentdashboard/content/CourseViewPage/modals/index.ts
+++ src/pages/studentdashboard/content/CourseViewPage/modals/index.ts
@@ -5,3 +5,4 @@
 
 export { default as InstructionsModal } from './InstructionsModal';
 export { default as SubmitModal } from './SubmitModal';
+export { default as AddTaskModal } from './AddTaskModal';
